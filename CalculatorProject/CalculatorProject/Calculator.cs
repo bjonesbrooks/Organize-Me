96242edef129d3a5032358350db88f30f9102f65
Bernice <Bernice@KYLE>
2017-07-27 19:02:54 -0500

    deleted form1 and form1designer added calculator

---
+++ CalculatorProject/CalculatorProject/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Calculator
+{
+    int numberone;
+    int numbertwo;
+
+    int main()
+    {
+        Console.WriteLine("What is the first number?");
+        numberone = Console.ReadLine();
+        Console.WriteLine("What is the second number?");
+        numbertwo = Console.ReadLine();
+        int sum;
+        sum = numberone + numbertwo;
+        Console.WriteLine(sum);
+
+
+    }
+    //public void plus() { }
+    //public void minus() { }
+    //public void times() { }
+    //public void divide() { }
+
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrganizeMe
+{
+    public partial class Calendar // :Form
+    {
+        string name;
+        int size;
+        string month;
+
+        string defaultName = "calendar";
+        int defaultSize = 100;
+
+        public Calendar()
+        {
+            this.setName(defaultName);
+            this.setSize(defaultSize);
+            this.setMonth("January");
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void clearCalendar()
+        {
+            //something that clears the calendar
+        }
+
+        private void setMonth(string m)
+        {
+            month = m;
+        }
+
+        private void sendMessage(string s)
+        {
+            print(s);
+        }
+
+        void main()
+        {
+            Calendar NorthHills = new Calendar();
+            bob.setName("North Hills");
+            bob.setMonth("July");
+
+            bob.sendMessage("Order a building inspection for" + bob.name);
+        }
+    }
+}
